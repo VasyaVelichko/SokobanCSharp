610bc7ddea55b66a6f7b94f05d69724ff1335f50
Rob Prouse <rob.prouse@bentley.com>
2014-01-07 16:22:53 -0500

    You can now make moves and push boxes

--- Sokoban.Test/BoardTest.cs
+++ Sokoban.Test/BoardTest.cs
@@ -14,5 +14,35 @@ namespace Alteridem.Sokoban.Test
          board.Load(boardStr);
          Assert.AreEqual( expected, board.ToString() );
       }
+
+      [TestCase( "", Move.Down, false )]
+      [TestCase( "#####\n#@$.#\n#####", Move.Down, false )]
+      [TestCase( "#####\n#@$.#\n#####", Move.Up, false )]
+      [TestCase( "#####\n#@$.#\n#####", Move.Right, true )]
+      [TestCase( "#####\n#@$.#\n#####", Move.Left, false )]
+      [TestCase( "#######\n#.@ # #\n#$* $ #\n#   $ #\n# ..  #\n#  *  #\n#######", Move.Up, false )]
+      [TestCase( "#######\n#.@ # #\n#$* $ #\n#   $ #\n# ..  #\n#  *  #\n#######", Move.Down, true )]
+      [TestCase( "#######\n#.@ # #\n#$* $ #\n#   $ #\n# ..  #\n#  *  #\n#######", Move.Right, true )]
+      [TestCase( "#######\n#.@ # #\n#$* $ #\n#   $ #\n# ..  #\n#  *  #\n#######", Move.Left, true )]
+      public void TestValidMove( string boardStr, Move move, bool expected )
+      {
+         var board = new Board( );
+         board.Load( boardStr );
+         bool valid = board.IsMoveValid( move );
+         Assert.AreEqual( expected, valid );
+      }
+
+      [TestCase( "#####\r\n#@$.#\r\n#####", Move.Right, "#####\r\n# @*#\r\n#####" )]
+      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Down, "#######\r\n#.  # #\r\n#$+ $ #\r\n# $ $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
+      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Right, "#######\r\n#. @# #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
+      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Left, "#######\r\n#+  # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
+      public void TestMove( string boardStr, Move move, string expected )
+      {
+         var board = new Board();
+         board.Load( boardStr );
+         bool valid = board.MakeMove( move );
+         Assert.IsTrue( valid );
+         Assert.AreEqual( expected, board.ToString() );
+      }
    }
 }
