cabb7c6e20c63b4c27eec1a00a0ff42f640c3fe8
Rob Prouse <rob@prouse.org>
2014-01-07 19:36:19 -0500

    Simple refactor of the board tests to add some contants

--- Sokoban.Test/BoardTest.cs
+++ Sokoban.Test/BoardTest.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Alteridem.Sokoban.Test
 {
    [TestFixture]
    public class BoardTest
    {
+      private const string SIMPLE_BOARD = "#####\r\n#@$.#\r\n#####";
+      private const string MEDIUM_BOARD = "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######";
+
       [TestCase( "", "" )]
-      [TestCase( "#####\r\n#@$.#\r\n#####", "#####\r\n#@$.#\r\n#####" )]
-      [TestCase( "#####\n#@$.#\n#####", "#####\r\n#@$.#\r\n#####" )]
+      [TestCase( SIMPLE_BOARD, SIMPLE_BOARD )]
+      [TestCase( "#####\n#@$.#\n#####", SIMPLE_BOARD )]
       public void TestLoad( string boardStr, string expected )
       {
          var board = new Board();
@@ -17,14 +19,14 @@ namespace Alteridem.Sokoban.Test
       }
 
       [TestCase( "", Move.Down, false, "" )]
-      [TestCase( "#####\r\n#@$.#\r\n#####", Move.Down, false, "" )]
-      [TestCase( "#####\r\n#@$.#\r\n#####", Move.Up, false, "" )]
-      [TestCase( "#####\r\n#@$.#\r\n#####", Move.Right, true, "#####\r\n# @*#\r\n#####" )]
-      [TestCase( "#####\r\n#@$.#\r\n#####", Move.Left, false, "" )]
-      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Up, false, "" )]
-      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Down, true, "#######\r\n#.  # #\r\n#$+ $ #\r\n# $ $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
-      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Right, true, "#######\r\n#. @# #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
-      [TestCase( "#######\r\n#.@ # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######", Move.Left, true, "#######\r\n#+  # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
+      [TestCase( SIMPLE_BOARD, Move.Down, false, "" )]
+      [TestCase( SIMPLE_BOARD, Move.Up, false, "" )]
+      [TestCase( SIMPLE_BOARD, Move.Right, true, "#####\r\n# @*#\r\n#####" )]
+      [TestCase( SIMPLE_BOARD, Move.Left, false, "" )]
+      [TestCase( MEDIUM_BOARD, Move.Up, false, "" )]
+      [TestCase( MEDIUM_BOARD, Move.Down, true, "#######\r\n#.  # #\r\n#$+ $ #\r\n# $ $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
+      [TestCase( MEDIUM_BOARD, Move.Right, true, "#######\r\n#. @# #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
+      [TestCase( MEDIUM_BOARD, Move.Left, true, "#######\r\n#+  # #\r\n#$* $ #\r\n#   $ #\r\n# ..  #\r\n#  *  #\r\n#######" )]
       public void TestMove( string boardStr, Move move, bool expectedValid, string expected )
       {
          var board = new Board();
